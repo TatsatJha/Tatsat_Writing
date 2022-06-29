4cd89eaab88c36429ecc66a75e9c64c091c33e09
TatsatJha <TatsatTarun@gmail.com>
2022-06-29 15:57:26 -0700

    make the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Montserrat": ['Montserrat']
+      },
+      keyframes: {
+        fadeUp: {
+          "0%": {transform:"translateY(-1.5rem)", opacity:"0%"},
+          "100%": {opacity:"100%"}
+        },
+        fadeIn: {
+          "0%": {opacity:"0%"},
+          "100%": {opacity:"100%"}
+        },
+      },
+      animation: {
+        fadeUp: 'fadeUp 1.25s 1 ease-in-out',
+        fadeIn: 'fadeIn .75s 1 ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
